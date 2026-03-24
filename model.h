f8b119439833aa507f74e0de1986b116579015b1
heyeepc <2675458141@qq.com>
2026-03-24 14:23:44 +0800

    Update model.h

--- model.h
+++ model.h
@@ -1,16 +1,43 @@
-
 #ifndef TRAIN_MODEL_H
 #define TRAIN_MODEL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // --- 界面显示模板 ---
 #define HEADER1 "------------ BOOK TICKET --------------\n"
 #define HEADER2 "| number | start city | reach city | takeofftime | receivetime | price | ticketnumber |\n"
 #define FORMAT  "|%-10s|%-10s|%-10s|%-10s|%-10s|%5d|  %5d   |\n"
-// 注意：DATA 宏通常配合链表指针 p 使用，确保你使用它时，当前函数里确实有名为 p 的指针
 #define DATA p->data.num, p->data.startcity, p->data.reachcity, p->data.takeofftime, p->data.receivetime, p->data.price, p->data.ticketnum
 
-// --- 结构体定义 ---
-// 这里接着写书上的 struct Train {...} 等内容
+// --- 基础数据结构 ---
+struct train {
+    char num[10]; //列车号
+    char startcity[10];  //出发城市
+    char reachcity[10]; //目标城市
+    char takeofftime[10]; //发车时间
+    char receivetime[10];  //到达时间
+    int price;  //票价
+    int ticketnum;  //票数
+};
 
+struct man {
+    char num[10]; //id
+    char name[10];  //姓名
+    int booknum;  // 订的票数
+};
+
+// --- 链表节点定义 ---
+typedef struct train_node {
+    struct train data;
+    struct train_node *next;
+} TrainNode, *TrainLink;
+
+typedef struct man_node {
+    struct man data;
+    struct man_node *next;
+} ManNode, *ManLink;
 
 #endif
+
