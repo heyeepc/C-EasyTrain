43ed6acfdf7a4e6815e13580c58657d1873abb9f
heyeepc <2675458141@qq.com>
2026-04-07 20:57:17 +0800

    更新

--- main.c
+++ main.c
@@ -15,6 +15,106 @@ void menu()
     puts("\t\t|----------------------------------------------------------------------|");
 }
 
+void printheader() {
+    printf(HEADER1);
+    printf(HEADER2);
+    printf(HEADER3);
+
+}
+
+void printdata( Node *Q) {
+    Node*p;
+    p=q;
+    printf(FORMAT,DATA);
+}
+
+void Traininfo(Link linkhead)
+{
+    struct node *p ,*r ,*s;;
+    char num[10];
+    r = linkhead;
+    s = linkhead->next;
+    while (r->next!=NULL)
+        r=r->next;
+    while (1) {
+
+        printf("please input the number of the train(0-return):");
+        scanf("%s",num);
+        if (strcmp(num,"0")==0)
+            break;
+        while (s) {
+            if (strcmp(s->name,num)==0) {
+                printf("the train ‘%s,is existing !\n",num);
+                return;
+            }
+            s = s->next;
+        }
+        p = (struct node *)malloc(sizeof(struct node));
+        strcpy(p->date,num,num);
+        printf("input the clit where the train will start");
+        scanf("%s",p->date.startcity);
+        printf("input the city where the train will reach");
+        scanf("%s",p->date.reachcity);
+        printf("input the time which the train take off");
+        scanf("%s",p->date.takeoffime);
+        printf("input the time which the train erceive");
+        scanf("%s",p->date.receivetime);
+        printf("input the price of the train");
+        scanf("%s",p->date.price);
+        printf("input the number of booked tickets:");
+        scanf("%d",&p->data.ticketnum);
+        p->next=NULL;
+        r->next=p;
+        r=p;
+        saveflag=1;
+    }
+}
+
+void searchtrain(Link I) {
+    Node *S[10],*R;
+    int sel,k,i=0;
+    char str1[5],str2[10];
+    if (!I->next) {
+        printf("there is not any record!");
+        return ;
+    }
+    printf("choose the way:\n1:according to the number of train ;\n2:according to the city \n ");
+    scanf("%d",&sel);
+    if (sel==1) {
+        printf("input the number of the train :");
+        scanf("%d",&str1);
+        r=I->next;
+        while (r!=NULL) {
+            if (strcmp(r->date.num,str1)==0) {
+                s[i]=r;
+                i++;
+                break;
+            }
+            else
+                r=r->next;
+        }
+    }
+    else if (sel==2) {
+        printf("input the city you want to go: ");
+        scanf("%s",str2);
+        r=I->next;
+        while (r!=NULL)
+            if (strcmp(r->date.startcity,str2)==0) {
+                s[i]=r;
+                i++;
+                r=r->next;
+            }
+            else
+                r=r->next;
+    }
+    if (i==0)
+        printf("can not find!");
+    else {
+        printheader();
+        for (k=0;k<i;k++);
+        printdata(s[k]);
+    }
+}
 
 
 int main(){
@@ -116,95 +216,6 @@ int main(){
 
 
 };
-
-void Traininfo(Link linkhead)
-{
-    struct node *p ,*r ,*s;;
-    char num[10];
-    r = linkhead;
-    s = linkhead->next;
-    while (r->next!=NULL)
-        r=r->next;
-    while (1) {
-
-        printf("please input the number of the train(0-return):");
-        scanf("%s",num);
-        if (strcmp(num,"0")==0)
-            break;
-        while (s) {
-            if (strcmp(s->name,num)==0) {
-                printf("the train ‘%s,is existing !\n",num);
-                return;
-            }
-            s = s->next;
-        }
-        p = (struct node *)malloc(sizeof(struct node));
-        strcpy(p->date,num,num);
-        printf("input the clit where the train will start");
-        scanf("%s",p->date.startcity);
-        printf("input the city where the train will reach");
-        scanf("%s",p->date.reachcity);
-        printf("input the time which the train take off");
-        scanf("%s",p->date.takeoffime);
-        printf("input the time which the train erceive");
-        scanf("%s",p->date.receivetime);
-        printf("input the price of the train");
-        scanf("%s",p->date.price);
-        printf("input the number of booked tickets:");
-        scanf("%d",&p->data.ticketnum);
-        p->next=NULL;
-        r->next=p;
-        r=p;
-        saveflag=1;
-    }
-}
-
-void searchtrain(Link I) {
-    Node *S[10],*R;
-    int sel,k,i=0;
-    char str1[5],str2[10];
-    if (!I->next) {
-        printf("there is not any record!");
-        return ;
-    }
-    printf("choose the way:\n1:according to the number of train ;\n2:according to the city \n ");
-    scanf("%d",&sel);
-    if (sel==1) {
-        printf("input the number of the train :");
-        scanf("%d",&str1);
-        r=I->next;
-        while (r!=NULL) {
-            if (strcmp(r->date.num,str1)==0) {
-                s[i]=r;
-                i++;
-                break;
-            }
-            else
-                r=r->next;
-        }
-    }
-    else if (sel==2) {
-        printf("input the city you want to go: ");
-        scanf("%s",str2);
-        r=I->next;
-        while (r!=NULL)
-            if (strcmp(r->date.startcity,str2)==0) {
-                s[i]=r;
-                i++;
-                r=r->next;
-            }
-            else
-                r=r->next;
-    }
-    if (i==0)
-        printf("can not find!");
-    else {
-        printheader();
-        for (k=0;k<i;k++);
-        printdata(s[k]);
-    }
-}
-
 
 
 
