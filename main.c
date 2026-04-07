87879e53295bcac0d649377d191bfd1e68872443
heyeepc <2675458141@qq.com>
2026-04-07 21:13:30 +0800

    更新

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h> // 用于 getch()
 #include "model.h"
+
+int saveflag = 0; // 全局变量定义
+
+// ... 你的 menu, printheader, printdata 函数放在这里 ...
 void menu()
 {
     puts("\n\n");
@@ -15,59 +22,54 @@ void menu()
     puts("\t\t|----------------------------------------------------------------------|");
 }
 
-void printheader() {
-    printf(HEADER1);
-    printf(HEADER2);
-    printf(HEADER3);
-
-}
-
-void printdata( Node *Q) {
-    Node*p;
-    p=q;
-    printf(FORMAT,DATA);
-}
-
-void Traininfo(Link linkhead)
-{
-    struct node *p ,*r ,*s;;
+void Traininfo(Link linkhead) {
+    Node *p, *r;
     char num[10];
     r = linkhead;
-    s = linkhead->next;
-    while (r->next!=NULL)
-        r=r->next;
-    while (1) {
+    while (r->next != NULL) r = r->next; // 移到链表末尾
 
-        printf("please input the number of the train(0-return):");
-        scanf("%s",num);
-        if (strcmp(num,"0")==0)
-            break;
+    while (1) {
+        printf("Input train number (0-return): ");
+        scanf("%s", num);
+        if (strcmp(num, "0") == 0) break;
+
+        // 查重逻辑
+        Node *s = linkhead->next;
+        int exists = 0;
         while (s) {
-            if (strcmp(s->name,num)==0) {
-                printf("the train ‘%s,is existing !\n",num);
-                return;
+            if (strcmp(s->data.num, num) == 0) {
+                printf("Error: Train %s already exists!\n", num);
+                exists = 1;
+                break;
             }
             s = s->next;
         }
-        p = (struct node *)malloc(sizeof(struct node));
-        strcpy(p->date,num,num);
-        printf("input the clit where the train will start");
-        scanf("%s",p->date.startcity);
-        printf("input the city where the train will reach");
-        scanf("%s",p->date.reachcity);
-        printf("input the time which the train take off");
-        scanf("%s",p->date.takeoffime);
-        printf("input the time which the train erceive");
-        scanf("%s",p->date.receivetime);
-        printf("input the price of the train");
-        scanf("%s",p->date.price);
-        printf("input the number of booked tickets:");
-        scanf("%d",&p->data.ticketnum);
-        p->next=NULL;
-        r->next=p;
-        r=p;
-        saveflag=1;
+        if (exists) continue;
+
+        p = (Node *)malloc(sizeof(Node));
+        strcpy(p->data.num, num); // 修正拼写和参数
+
+        printf("Start City: "); scanf("%s", p->data.startcity);
+        printf("Reach City: "); scanf("%s", p->data.reachcity);
+        printf("Takeoff Time: "); scanf("%s", p->data.takeofftime);
+        printf("Price: "); scanf("%d", &p->data.price); // 必须是 %d 和 &
+        printf("Tickets: "); scanf("%d", &p->data.ticketnum);
+
+        p->next = NULL;
+        r->next = p;
+        r = p;
+        saveflag = 1;
     }
+}
+
+void printheader() {
+    printf(HEADER1); // 这里的 HEADER1 展开后自带引号
+    printf(HEADER2);
+}
+
+void printdata(Node *p) {
+    // DATA 宏会自动展开为 p->data.num, p->data.startcity...
+    printf(FORMAT, DATA);
 }
 
 void searchtrain(Link I) {
@@ -116,106 +118,76 @@ void searchtrain(Link I) {
     }
 }
 
-
-int main(){
-    FILE *fp1,*fp2;
-    Node *p,*r;
-    char ch1,ch2;
+int main() {
+    Node *p, *r;
     Link l;
     bookLink k;
-    book *t,*h;
+    book *t, *h;
     int sel;
-    l=(Node*)malloc(sizeof(Node));
-    l->next=NULL;
-    r=l;
-    k=(book*) malloc(sizeof (book));
-    k->next=NULL;
-    h=k;
-    fp1= fdopen("f:\\train.txt","ab+");
-    if(fp1==NULL)
-    {
-        printf("can't open the file!");
-        return 0;
-    }
-    while (!feof(fp1))
-    {
-        p=(Node*) malloc(sizeof (Node));
-        if (fread(p,sizeof (Node),1,fp1)==1)
-        {
-            p->next=NULL;
-            r->next=p;
-            r=p;
+
+    // --- 初始化链表头结点 ---
+    l = (Node*)malloc(sizeof(Node));
+    l->next = NULL;
+    r = l;
+
+    k = (bookLink)malloc(sizeof(book));
+    k->next = NULL;
+    h = k;
+
+    // --- 文件读取逻辑 (建议改用 fopen) ---
+    FILE *fp1 = fopen("train.txt", "rb+");
+    if (fp1 != NULL) {
+        while (!feof(fp1)) {
+            p = (Node*)malloc(sizeof(Node));
+            if (fread(p, sizeof(Node), 1, fp1) == 1) {
+                p->next = NULL;
+                r->next = p;
+                r = p;
+            } else {
+                free(p); // 读取失败释放内存
+            }
         }
+        fclose(fp1);
     }
-    fclose(fp1);
-    fp2= fdopen("f:\\man.txt","ab+");
-    if(fp2==NULL)
-    {
-        printf("can't opem the file!");
-        return 0;
-    }
-    while (!feof(fp2))
-    {
-        t = (book*) malloc(sizeof (book));
-        if (fread(t,sizeof (book),1,fp2)==1)
-        {
-            t->next=NULL;
-            h->next=t;
-            h=t;
-        }
-    }
-    fclose(fp2);
-    while (1)
-    {
+
+    // --- 主循环 ---
+    while (1) {
         system("CLS");
         menu();
-        printf("\tplease choose(0~6)");
-        scanf("%d",&sel);
+        printf("\tPlease choose (0~6): ");
+        if (scanf("%d", &sel) != 1) { // 防止输入非数字导致死循环
+            while (getchar() != '\n');
+            continue;
+        }
         system("CLS");
-        if(sel==0)
-        {
-            if(saveflag==1)
-            {
-                getchar();
-                printf("\nthe file have been changed! do you want to save it (y/n)?\n ");
-                scanf("%c",&ch1);
-                if(ch1=="y"||ch1=="y");
-                {
-                    SaveBookinfo(k);
-                    SaveTrainlnfo(l);
+
+        if (sel == 0) {
+            if (saveflag == 1) {
+                char ch1;
+                printf("\nFile changed! Save it? (y/n): ");
+                scanf(" %c", &ch1); // 注意 %c 前有一个空格，用来消耗回车
+                if (ch1 == 'y' || ch1 == 'Y') { // 必须用单引号
+                    SaveTrainInfo(l); // 确保函数名拼写一致
+                    SaveBookInfo(k);
                 }
             }
-            printf("\nThank you are welcome too");
+            printf("\nThank you for using!\n");
             break;
-            
         }
 
+        // 将 switch 移入循环内部
         switch (sel) {
-            case 1:
-                Traininfo(l);
-                break;
-            case 2:
-                Searchtrain(l);
-                break;
-            case 3:
-                B00kticket(l,k);
-                break;
-            case 4:
-                MODIFY(l);
-                break;
-            case 5:
-                showtrain(l);
-                break;
-            case 6:
-                SaveTrainlnfo(l);SaveBookinfo(k);
-                break;
-            case 0:
-                return 0;
+            case 1: Traininfo(l); break;
+            case 2: searchtrain(l); break;
+            case 3: B00kticket(l, k); break;
+            case 5: showtrain(l); break;
+            case 6: /* 执行保存逻辑 */; break;
+            default: printf("Invalid choice!"); break;
         }
+        printf("\nPress any key to continue...");
+        getch();
     }
-    printf("\nplease press any key to continue");
-    getch();
-};
-
+    return 0;
+}
 
 
